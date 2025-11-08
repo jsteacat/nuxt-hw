e22efaf447ac087327f379d7727506e059b28db6
Stepan Kormilin <ubscoder@gmail.com>
2025-11-08 23:39:02 +0300

    add routing && ActionButton component && 404 error page

---
+++ app/pages/post/[id]/edit.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+const route = useRoute()
+const postId = route.params.id
+</script>
+
+<template>
+  <div>EDIT POST {{postId}}</div>
+</template>
---
+++ app/pages/post/[id]/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+const route = useRoute()
+const postId = route.params.id
+</script>
+
+<template>
+  <div>POST {{postId}} PAGE</div>
+</template>
